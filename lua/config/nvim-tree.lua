bfeabe7441274f05652a274271717d41ba1feb46
ddzizz <ding_r@foxmail.com>
2023-02-14 01:15:25 +0800

    fix nvim-tree config

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -7,7 +7,7 @@ require("nvim-tree").setup({
 	},
 	sync_root_with_cwd = true,
 	respect_buf_cwd = true,
-	update_focuesd_file = {
+	update_focused_file = {
 		enable = true,
 		update_root = true,
 	},
