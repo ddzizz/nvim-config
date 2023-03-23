aad5577fd28e5bd9974d6f6659494d2cb5458fd9
ddzizz <ding_r@foxmail.com>
2023-03-23 11:38:34 +0800

    add shotcut to open current buffer path by explorer

--- init.lua
+++ init.lua
@@ -1,7 +1,11 @@
+local vim = vim
+local keymap = vim.keymap.set
+local noremap_n_slient = { noremap = true, silent = true }
+
 require('basic')
 require('plugins')
 require('theme')
 
 
-
+keymap('n', '<leader>bo', function() vim.api.nvim_command('silent !cmd /c start "" "' .. vim.fn.expand('%:p:h') .. '"') end, noremap_n_slient)
 
