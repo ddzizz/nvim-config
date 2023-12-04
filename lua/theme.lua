97d436bdd81ac4f3d18fd981f721d5a133d10f7f
ddzizz <ding_r@foxmail.com>
2023-12-04 10:25:50 +0800

    use new theme

--- lua/theme.lua
+++ lua/theme.lua
@@ -3,9 +3,9 @@ if vim.g.vscode then
 else
 	-- vim.cmd("colorscheme github_dark")
 	-- vim.cmd("colorscheme nordic")
-	-- vim.cmd("colorscheme onedark")
+	vim.cmd("colorscheme onedark")
 	-- vim.cmd("colorscheme tokyonight")
-	vim.cmd("colorscheme tokyodark")
+	-- vim.cmd("colorscheme tokyodark")
 	-- vim.cmd("colorscheme catppuccin")
 	-- vim.cmd("colorscheme nord")
 end
