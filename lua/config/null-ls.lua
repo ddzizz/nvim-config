49b6f4e00c3cab1d7b95f156cdd6aeb6b06f8ebd
ddzizz <ding_r@foxmail.com>
2024-08-19 14:04:03 +0800

    add clang format

--- lua/config/null-ls.lua
+++ lua/config/null-ls.lua
@@ -9,6 +9,7 @@ null_ls.setup({
 		null_ls.builtins.formatting.golines,
 		null_ls.builtins.formatting.csharpier,
 		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.clang_format,
 		null_ls.builtins.diagnostics.eslint,
 		null_ls.builtins.completion.spell,
 		-- require("none-ls.diagnostics.eslint"),
