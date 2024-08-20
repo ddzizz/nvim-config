cbb52167e6188eaa7d7d56334e0c089ddc4f7232
ddzizz <ding_r@foxmail.com>
2024-08-20 12:07:49 +0800

    change clang_format and csharpier position

--- lua/config/null-ls.lua
+++ lua/config/null-ls.lua
@@ -3,15 +3,15 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
 	sources = {
+		null_ls.builtins.formatting.clang_format,
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.goimports,
 		null_ls.builtins.formatting.goimports_reviser,
 		null_ls.builtins.formatting.golines,
-		null_ls.builtins.formatting.csharpier,
 		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.formatting.clang_format,
 		null_ls.builtins.diagnostics.eslint,
 		null_ls.builtins.completion.spell,
+		null_ls.builtins.formatting.csharpier,
 		-- require("none-ls.diagnostics.eslint"),
 	},
 	on_attach = function(client, bufnr)
