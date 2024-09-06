1b1e210664bc88a6c00fca40d51b98ec1ed18634
ddzizz <ding_r@foxmail.com>
2024-09-06 12:04:58 +0800

    add avante plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -401,6 +401,99 @@ if not vim.g.vscode then
 		-- Formatter
 		 'mhartington/formatter.nvim'
 		]]
+		-- AI
+		--
+
+		{
+			"yetone/avante.nvim",
+			event = "VeryLazy",
+			lazy = false,
+			version = false, -- set this if you want to always pull the latest change
+			opts = {
+				debug = true,
+				-- provider = "claude", -- Recommend using Claude
+				-- claude = {
+				-- 	endpoint = "https://api.gptsapi.net",
+				-- 	model = "claude-3-5-sonnet-20240620",
+				-- 	-- proxy = "http://192.168.2.217:7890",
+				-- 	-- timeout = "10000",
+				-- },
+				provider = "wildcard", -- Recommend using Claude
+				vendors = {
+					wildcard = {
+						-- endpoint = "https://api.anthropic.com",
+						endpoint = "https://api.gptsapi.net/v1/chat/completions",
+						model = "claude-3-5-sonnet-20240620",
+						api_key_name = "OPENAI_API_KEY",
+						parse_curl_args = function(opts, code_opts)
+							return {
+								url = "https://api.gptsapi.net/v1/chat/completions",
+								headers = {
+									["Accept"] = "application/json",
+									["Content-Type"] = "application/json",
+									["Authorization"] = "Bearer sk-XiU91c9c8829eb1cf8055e15d4a391ea1758ea7ed35swfAA",
+								},
+								body = {
+									model = "claude-3-5-sonnet-20240620",
+									messages = { -- you can make your own message, but this is very advanced
+										{ role = "system", content = code_opts.system_prompt },
+										{
+											role = "user",
+											content = require("avante.providers.openai").get_user_message(code_opts),
+										},
+									},
+									temperature = 0,
+									-- max_tokens = 8192,
+									stream = true, -- this will be set by default.
+								},
+							}
+						end,
+						parse_response_data = function(data_stream, event_state, opts)
+							require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+						end,
+					},
+				},
+			},
+			-- config = function()
+			-- 	require("config.avante")
+			-- end,
+			-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+			build = "make",
+			-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+			dependencies = {
+				"stevearc/dressing.nvim",
+				"nvim-lua/plenary.nvim",
+				"MunifTanjim/nui.nvim",
+				--- The below dependencies are optional,
+				"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+				"zbirenbaum/copilot.lua", -- for providers='copilot'
+				{
+					-- support for image pasting
+					"HakonHarnes/img-clip.nvim",
+					event = "VeryLazy",
+					opts = {
+						-- recommended settings
+						default = {
+							embed_image_as_base64 = false,
+							prompt_for_file_name = false,
+							drag_and_drop = {
+								insert_mode = true,
+							},
+							-- required for Windows users
+							use_absolute_path = true,
+						},
+					},
+				},
+				{
+					-- Make sure to set this up properly if you have lazy=true
+					"MeanderingProgrammer/render-markdown.nvim",
+					opts = {
+						file_types = { "markdown", "Avante" },
+					},
+					ft = { "markdown", "Avante" },
+				},
+			},
+		},
 
 		-- 终端
 		{
